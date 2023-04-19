0e34215b35e9db91334f1a8df7f55c696772f925
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-04-19 19:11:23 +0100

    moved origin story to home page, altered headingf for guild section

--- src/components/pages/Guild/Guild.js
+++ src/components/pages/Guild/Guild.js
@@ -6,36 +6,11 @@ function Guild() {
         <div className="guild">
           <br/>
           <div>
-          <h1>Guild</h1>
+          <h1>The Four Quarters</h1>
           </div>
           <div>
           {/* <img className="card-img-top " src={image} alt="image of the guild" /> */}
           </div>
-          <br/>
-          <div>
-            <h3>Origin Story</h3>
-          <p className="originStory">
-            <br/>
-            {/* Here I will create an information section for the guild (possibly with links, this info might be moved to home page) */}
-            We were founded in the year 406 by Darek Dawnbreaker, Stabitha Christie, Tink Wildwander and Professor Orzanin,<br/>
-            following their lucrative capture of the Dwarven bandit chief Hulil Lutan.<br/> 
-            The Warsong Order is a private military company offering protection, investigation, policing, <br/>
-            and other services across the entire planet of Amurru to any level of client, private or otherwise. <br/>
-            <br/>
-            Though starting off small, our initial operations were such a resounding success that various monarchies <br/>
-            and governing bodies across the globe began to solicit our services regularly, leading to the grand and powerful <br/>
-            institution that the Order is today. We now enjoy a vast amount of global influence, <br/>
-            but strongly disavow any political or moral alignment other than those held by each individual member. <br/>
-            <br/>
-            Our grand centre of operations resides on the continent of Tal’Dorei, in the city of Emon. <br/>
-            The main hall extends into four separate forks collected in groups of three, named after the four cardinal directions. <br/>
-            The North, East, South and West Forks all cater to different needs and house varying facilities. <br/>
-            These forks are better know as The Barracks, The Shadow Sect, The Hunt, and The Arcana Academy respectively. 
-            {/* maybe make the last line sections clickable? */}
-          </p>
-          </div>
-          <br/>
-          <h2>The Four Quarters</h2>
           <br/>
           <div>
             <h3>The Barracks (The War Quarter)</h3>
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -14,22 +14,28 @@ function Home() {
       {/* <img className="card-img-top portrait" src={image} alt="me" /> */}
       </div>
       <br/>
-    {/* Home page:
-    
-    This should be a welcome landing page that contains:
-    
-    Your Name - Done
-    
-    A headshot of you (or an avatar)
-    
-    Your brand statement - Done
-    
-    Some indication that this is your portfolio site - Done */}
-      <div>
-      <p className="brandStatement">
-        The welcome page for the WSO 
-      </p>
-      </div>
+          <div>
+            <h3>Origin Story</h3>
+          <p className="originStory">
+            <br/>
+            {/* Here I will create an information section for the guild (possibly with links, this info might be moved to home page) */}
+            We were founded in the year 406 by Darek Dawnbreaker, Stabitha Christie, Tink Wildwander and Professor Orzanin,<br/>
+            following their lucrative capture of the Dwarven bandit chief Hulil Lutan.<br/> 
+            The Warsong Order is a private military company offering protection, investigation, policing, <br/>
+            and other services across the entire planet of Amurru to any level of client, private or otherwise. <br/>
+            <br/>
+            Though starting off small, our initial operations were such a resounding success that various monarchies <br/>
+            and governing bodies across the globe began to solicit our services regularly, leading to the grand and powerful <br/>
+            institution that the Order is today. We now enjoy a vast amount of global influence, <br/>
+            but strongly disavow any political or moral alignment other than those held by each individual member. <br/>
+            <br/>
+            Our grand centre of operations resides on the continent of Tal’Dorei, in the city of Emon. <br/>
+            The main hall extends into four separate forks collected in groups of three, named after the four cardinal directions. <br/>
+            The North, East, South and West Forks all cater to different needs and house varying facilities. <br/>
+            These forks are better know as The Barracks, The Shadow Sect, The Hunt, and The Arcana Academy respectively. 
+            {/* maybe make the last line sections clickable? */}
+          </p>
+          </div>
     </div>
   );
 }
